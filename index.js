b33e6d9750cb3a2c9ac4a6038797420c66605538
pathikroy86 <pathik.roy86@gmail.com>
2026-06-15 15:06:08 +0600

    final commit

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const getLoggedInUserId = (req) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         const db = client.db("ideahub");
         const ideascollection = db.collection("startupIdeas");
         const commentsCollection = db.collection("comments");
@@ -283,7 +283,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        //await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
